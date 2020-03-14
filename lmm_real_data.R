b86420090a907f51c20c64b865fc2e50f6d8af1d
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2020-03-13 20:57:15 -0400

    big cleanup, minor fixes for Yiqis real data script

--- lmm_real_data.R
+++ lmm_real_data.R
@@ -1,85 +1,49 @@
-# NOTES:
-# - the time will also be precomputed this time, so we should consider its different values on different machines
-# - LFA doesn't work with BEDMatrix.  Omitted for now
 setwd("/dscrhome/yy222/gas-rgls-master/scripts1")
+#setwd("../scripts")
 library(optparse)    # for terminal options
-library(genio)       # to write BED files for external software
+library(genio)       # read_fam
 library(BEDMatrix)   # to load real genotypes with low memory usage
-library(popkin)      # to estimate kinship
+library(popkin)      # to estimate kinship (for simtrait)
 library(popkinsuppl) # for kinship_std (supports BEDMatrix and missing data, unlike the crappy local implementation)
-library(readr)       # to write kinship matrix
-library(tibble)      # to store data
-library(lfa)         # GWAS gcatest
-source("gas_plots.R")
 source('paths.R')
-source('gas_lmm_gemma.R')
-source('gas_lmm_emmax.R')
 source('gas_lmm_gcta.R')
 source('kinship_to_evd.R')
-# load new functions from external scripts
-source('gas_lm_optim.R')
-source('gas_pca_optim.R')
-source('gas_rgls.R')
 
+gcta_bin<-"/hpc/group/biostat/yy222/gcta_1.92.4beta1/gcta64"
 
 setwd("/dscrhome/yy222/human-differentiation-manuscript-master/data")
+#setwd('../../human-differentiation-manuscript/data/')
 # define options
 option_list = list(
   make_option(c("-i", "--input"), type = 'character', default = "human_origins_and_pacific_public", 
-              help = "Path for input genotypes (BED/BIM/FAM files, excluding extension)"),
-  make_option("--debug", action = "store_true", default = FALSE, 
-              help = "debug mode (GCTA and GEMMA are fully verbose)")
+              help = "Path for input genotypes (BED/BIM/FAM files, excluding extension)")
 )
-
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
 # get values
 name_in <- opt$input
-if (is.na(name_in))
-  stop('`-i/--input` is mandatory!')
-debug <- opt$debug
 
 # save kinship data
 file_kinship_data <- paste0(name_in, '.RData')
 
-# HACK: before BEDMatrix, copy *-orig.fam to *.fam so it loads without issue
-file.copy(
-  paste0(name_in, '-orig.fam'),
-  paste0(name_in, '.fam')
-)
+message("GCTA")
+gas_lmm_gcta_kin(gcta_bin, name_in)
 
-# load and process genotypess
+# load and process genotypes
 # do once per run (these things don't change)
-# load genotypes
 message('BEDMatrix')
 X <- BEDMatrix(name_in)
-
-# estimate kinship, needed for accurate setting of heritability
-# for benchmarks, as this is part of RGLS, let's invert kinship too and time the whole thing
-
-# write TPED (for ancient EMMAX code)
-message('plink_bed_to_tped')
-plink_bed_to_tped(plink1_bin, name_in) # must be plink1 for now
 fam <- read_fam(name_in)
+
+message('popkin')
 kinship_estimate <- popkin(X, fam$fam)
-kinship_inv_estimate <- solve(kinship_estimate)
 
+message('kinship_std')
 kinship_estimate_old <- kinship_std(X) # estimate kinship the old way
-eigenvectors_estimate_old <- kinship_to_evd( kinship_estimate_old ) # get all eigenvalues
-
-# compute eigenvectors from popkin estimate (for PCA "oracle/popkin" versions)
 message('kinship_to_evd')
-eigenvectors_estimate <- kinship_to_evd(kinship_estimate)
+eigenvectors_estimate_old <- kinship_to_evd( kinship_estimate_old ) # get all eigenvalues
+# save PCA data
+message('writing: ', file_kinship_data)
+save(kinship_estimate, eigenvectors_estimate_old, file = file_kinship_data)
 
-# write popkin kinship matrix in correct format
-# shared by at least GEMMA and EMMAX
-file_popkin_kin <- paste0(name_in, '.popkin_kinship.txt')
-message('writing: ', file_popkin_kin)
-kinship_tibble <- as_tibble( 2 * kinship_estimate, .name_repair = 'minimal')
-write_tsv(kinship_tibble, file_popkin_kin, col_names = FALSE)
-
-# files to delete when all is done
-file_fam <- paste0(name_in, '.fam')
-# test that it's actually there!
-save(kinship_estimate, kinship_inv_estimate, file_popkin_kin, eigenvectors_estimate, eigenvectors_estimate_old, file = file_kinship_data)
