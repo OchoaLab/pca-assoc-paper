d8459d29f04542555cfa7aa1a42ee01a2728a934
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2020-08-12 11:16:47 -0400

    adapted batch submission loop script for `ochoalabtools` improvements

--- scripts/batch.R
+++ scripts/batch.R
@@ -4,7 +4,34 @@ library(ochoalabtools)
 script <- 'real-05-gcta.R'
 bfile <- 'hgdp_wgs_autosomes_ld_prune_1000kb_0.3'
 mem <- '4G'
-time <- NA
+
+# main submission steps
+# global vars: script, bfile, mem
+submit_rep_pcs <- function(
+                           rep,
+                           pcs
+                           ) {
+    # pass params to command
+    commands <- paste0(
+        'time Rscript ', script, ' --bfile ', bfile, ' -r ', rep, ' --n_pcs ', pcs
+    )
+    # give name clear params too
+    name <- paste0( 'r', rep, 'p', pcs )
+    
+    # create submission file
+    batch_writer(
+        commands,
+        name,
+        mem = mem
+    )
+
+    # submit job!
+    batch_submit( name )
+
+    # remove script when done (not needed after submission)
+    batch_cleanup( name )
+}
+    
 
 # setup loop
 reps <- 50
@@ -13,24 +40,6 @@ pcs_max <- 90
 # start loop
 for ( rep in 1 : reps ) {
     for ( pcs in 0 : pcs_max ) {
-        # pass params to command
-        commands <- paste0(
-            'time Rscript ', script, ' --bfile ', bfile, ' -r ', rep, ' --n_pcs ', pcs
-        )
-        # give name clear params too
-        name <- paste0( 'hgdp_gcta_r', rep, '_pc', pcs )
-        
-        # create submission file
-        batch_writer(
-            commands,
-            name,
-            mem = mem,
-            time = time
-        )
-
-        # submit job!
-        batch_submit(
-            name
-        )
+        submit_rep_pcs( rep, pcs )
     }
 }
