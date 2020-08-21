e75da4ddc20cb8863e7c46bbf644199bd02119fd
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2020-08-21 18:05:33 -0400

    PCA AUC-RMSD loop was parallelized (otherwise super slow on 1000 Genomes)

--- scripts/real-07-auc-rmsd.R
+++ scripts/real-07-auc-rmsd.R
@@ -4,6 +4,7 @@ library(optparse)
 library(readr)
 library(tibble)
 library(genio)
+library(parallel)
 
 # load new functions from external scripts
 dir_orig <- getwd()
@@ -33,7 +34,9 @@ option_list = list(
     make_option(c("-r", "--rep"), type = "integer", default = 50,
                 help = "Max replicates", metavar = "int"),
     make_option("--sim", action = "store_true", default = FALSE, 
-                help = "Genotypes are simulated (rather than real; alters location only)")
+                help = "Genotypes are simulated (rather than real; alters location only)"),
+    make_option(c("-t", "--threads"), type = "integer", default = detectCores(), 
+                help = "number of threads (default use all cores, but may consume excessive memory)", metavar = "int")
 )
 
 opt_parser <- OptionParser(option_list = option_list)
@@ -43,6 +46,7 @@ opt <- parse_args(opt_parser)
 name <- opt$bfile
 rep_max <- opt$rep
 n_pcs_max <- opt$n_pcs
+threads <- opt$threads
 
 # stop if name is missing
 if ( is.na(name) )
@@ -58,7 +62,77 @@ if (opt$sim)
 # get number of loci
 m_loci <- count_lines( name_in, 'bim' )
 
+# function to process one n_pcs
+# note every other parameter is a global variable
+auc_rmsd_one_pcs <- function(n_pcs) {
+    
+    # file to read
+    file_pvals <- paste0( 'pvals_', method, '_', n_pcs, '.txt.gz' )
+    file_sum <- paste0( 'sum_', method, '_', n_pcs, '.txt.gz' )
+    
+    # if output is already there, don't do anything (don't recalculate)
+    if ( file.exists( file_sum ) )
+        return()
+    # if there's no input file, also skip silently
+    if ( !file.exists( file_pvals ) )
+        return()
+    
+    # only report files processed in this run
+    message(
+        'rep: ', rep,
+        ', method: ', method,
+        ', pcs: ', n_pcs
+    )
+    
+    # read the file
+    pvals <- read_lines(
+        file_pvals,
+        na = 'NA' # use this to prevent warnings
+    )
+
+    # special case is a single line with the word NULL, which occurs particularly with GCTA outputs when model does not converge/is underdetermined
+    if ( length(pvals) == 1 && pvals == 'NULL' ) {
+        # when pvals are NULL, they get printed as an empty file
+        # this is what we get from pvals_to_null_rmsd, pvals_to_pr_auc if we had set pvals==NULL
+        rmsdp <- NA
+        aucpr <- NA
+        # this special case wasn't handled below, I think
+        lambda <- NA
+    } else {
+        # make sure length is correct!
+        if ( length(pvals) != m_loci )
+            stop( 'File has ', length(pvals), ' p-values, expected ', m_loci )
+
+        # convert strings to numeric
+        pvals <- as.numeric( pvals )
+        # calculate RMSD_p
+        rmsdp <- pvals_to_null_rmsd(pvals, causal_indexes)$rmsd
+        # calculate AUC_PR
+        aucpr <- pvals_to_pr_auc(pvals, causal_indexes)
+        # calculate inflation (but on p-values instead of Chi-Sq; this makes sense as not all stats are Chi-Sq anyway)
+        # NOTES:
+        # - not subsetting for true nulls (as done in practice)
+        # - exact inversion to chi-sq stat
+        lambda <- qchisq( 1 - median(pvals, na.rm = TRUE), df = df ) / x_m
+    }
+    # put everything into a tibble, with all the info we want conveniently in place
+    tib <- tibble(
+        method = method,
+        pc = n_pcs,
+        rep = rep,
+        rmsd = rmsdp,
+        auc = aucpr,
+        lambda = lambda
+    )
+    # save this one row to output file
+    write_tsv(
+        tib,
+        file_sum
+    )
+}
+
 for ( rep in 1 : rep_max ) {
+
     # move higher to the "reps" location
     # this is so GCTA's temporary files don't overwrite files from other parallel runs
     dir_out <- paste0( 'rep-', rep )
@@ -70,77 +144,18 @@ for ( rep in 1 : rep_max ) {
     # load trait info we need
     load( 'simtrait.RData' )
     # loads:
-    ## trait,
-    ## causal_indexes,
-    ## causal_coeffs
+    ## trait,          # not used
+    ## causal_indexes, # only one used
+    ## causal_coeffs   # not used
 
     # start a big loop
     for ( method in methods ) {
-        for ( n_pcs in 0 : n_pcs_max ) {
-            # file to read
-            file_pvals <- paste0( 'pvals_', method, '_', n_pcs, '.txt.gz' )
-            file_sum <- paste0( 'sum_', method, '_', n_pcs, '.txt.gz' )
-
-            # if output is already there, don't do anything (don't recalculate)
-            if ( file.exists( file_sum ) ) {
-                ## message( 'File already exists, skipping: ', file_sum )
-            } else if ( !file.exists( file_pvals ) ) {
-                ## message( 'No p-vals, skipping: ', file_pvals )
-            } else {
-                # only report files processed in this run
-                message(
-                    'rep: ', rep,
-                    ', method: ', method,
-                    ', pcs: ', n_pcs
-                )
-                
-                # read the file
-                pvals <- read_lines(
-                    file_pvals,
-                    na = 'NA' # use this to prevent warnings
-                )
-
-                # special case is a single line with the word NULL, which occurs particularly with GCTA outputs when model does not converge/is underdetermined
-                if ( length(pvals) == 1 && pvals == 'NULL' ) {
-                    # when pvals are NULL, they get printed as an empty file
-                    # this is what we get from pvals_to_null_rmsd, pvals_to_pr_auc if we had set pvals==NULL
-                    rmsdp <- NA
-                    aucpr <- NA
-                    # this special case wasn't handled below, I think
-                    lambda <- NA
-                } else {
-                    # make sure length is correct!
-                    if ( length(pvals) != m_loci )
-                        stop( 'File has ', length(pvals), ' p-values, expected ', m_loci )
-
-                    # convert strings to numeric
-                    pvals <- as.numeric( pvals )
-                    # calculate RMSD_p
-                    rmsdp <- pvals_to_null_rmsd(pvals, causal_indexes)$rmsd
-                    # calculate AUC_PR
-                    aucpr <- pvals_to_pr_auc(pvals, causal_indexes)
-                    # calculate inflation (but on p-values instead of Chi-Sq; this makes sense as not all stats are Chi-Sq anyway)
-                    # NOTES:
-                    # - not subsetting for true nulls (as done in practice)
-                    # - exact inversion to chi-sq stat
-                    lambda <- qchisq( 1 - median(pvals, na.rm = TRUE), df = df ) / x_m
-                }
-                # put everything into a tibble, with all the info we want conveniently in place
-                tib <- tibble(
-                    method = method,
-                    pc = n_pcs,
-                    rep = rep,
-                    rmsd = rmsdp,
-                    auc = aucpr,
-                    lambda = lambda
-                )
-                # save this one row to output file
-                write_tsv(
-                    tib,
-                    file_sum
-                )
-            }
-        }
+        # this is where parallelization happens!
+        mclapply(
+            0 : n_pcs_max,
+            auc_rmsd_one_pcs,
+            mc.cores = threads
+        )
     }
     
     # move back down when done with this rep
