2ddc5b0c61c6e4912be6f67a00b469303aa9af81
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2021-11-19 17:04:16 -0500

    batch: new version uses slurm arrays notation, appears to submit large jobs much faster!

--- scripts/batch.R
+++ scripts/batch.R
@@ -6,13 +6,13 @@ library(ochoalabtools)
 plink <- TRUE # FALSE
 fes <- FALSE # TRUE
 #bfile <- 'sim-n100-k10-f0.1-s0.5-g1'; short <- 's'
-bfile <- 'sim-n1000-k10-f0.1-s0.5-g1'; short <- 'l'
+#bfile <- 'sim-n1000-k10-f0.1-s0.5-g1'; short <- 'l'
 #bfile <- 'sim-n1000-k10-f0.1-s0.5-g20'; short <- 'f'
 #bfile <- 'HoPacAll_ld_prune_1000kb_0.3'; short <- 'h'
 #bfile <- 'hgdp_wgs_autosomes_ld_prune_1000kb_0.3_maf-0.01'; short <- 'd'
 #bfile <- 'all_phase3_filt-minimal_ld_prune_1000kb_0.3_maf-0.01'; short <- 'k'
 #bfile <- 'HoPacAll_ld_prune_1000kb_0.3_sim'; short <- 'H'
-#bfile <- 'hgdp_wgs_autosomes_ld_prune_1000kb_0.3_maf-0.01_sim'; short <- 'D'
+bfile <- 'hgdp_wgs_autosomes_ld_prune_1000kb_0.3_maf-0.01_sim'; short <- 'D'
 #bfile <- 'all_phase3_filt-minimal_ld_prune_1000kb_0.3_maf-0.01_sim'; short <- 'K'
 
 # needed to make sure each process gets enough memory if all my jobs saturate the machines
@@ -37,17 +37,15 @@ short <- paste0( short, if (plink) 'p' else 'g' )
 
 # main submission steps
 # global vars: script, bfile, short, mem, plink
-submit_rep_pcs <- function(
-                           rep,
-                           pcs
-                           ) {
+# new version uses array jobs, arraying over PCs
+submit_rep_pcs <- function( rep ) {
     # pass params to command
     # first do core set of parameters, more to be added below as needed
     commands <- paste0(
         'time Rscript ', script,
         ' --bfile ', bfile,
         ' -r ', rep,
-        ' --n_pcs ', pcs,
+        ' --n_pcs ', '$SLURM_ARRAY_TASK_ID',
         ' -t ', threads,
         ' --dcc'
     )
@@ -70,14 +68,15 @@ submit_rep_pcs <- function(
     }
     
     # give name clear params too
-    name <- paste0( short, rep, '-', pcs )
+    name <- paste0( short, rep, '-%a' )
     
     # create submission file
     batch_writer(
         commands,
         name,
         mem = mem,
-   	threads = threads
+   	threads = threads,
+        array = '0-90' # number of PCs hardcoded here
     )
 
     # submit job!
@@ -89,42 +88,22 @@ submit_rep_pcs <- function(
 
 # setup loop
 reps_max <- 50
-pcs_max <- 90
 
 # I
-# test one
+# test one rep
 rep <- 1
-pcs <- pcs_max
-submit_rep_pcs( rep, pcs )
+submit_rep_pcs( rep )
 
 ## # II
-## # finish rest of rep
-## rep <- 1
-## for ( pcs in 0 : ( pcs_max - 1 ) ) {
-##     submit_rep_pcs( rep, pcs )
-## }
-
-## # III
 ## # finish rest of reps
 ## for ( rep in 2 : reps_max ) {
-##     for ( pcs in 0 : pcs_max ) {
-##         submit_rep_pcs( rep, pcs )
-##     }
+##     submit_rep_pcs( rep )
 ## }
 
 ########################
 
-## # start loop (PCs on outside, as I was running plink locally)
-## for ( pcs in 0 : pcs_max ) {
-##     for ( rep in 1 : reps_max ) {
-##         submit_rep_pcs( rep, pcs )
-##     }
-## }
-
 ## # start loop
 ## for ( rep in 1 : reps_max ) {
-##     for ( pcs in 0 : pcs_max ) {
-##         submit_rep_pcs( rep, pcs )
-##     }
+##     submit_rep_pcs( rep )
 ## }
 
