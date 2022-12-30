6239b417d1506b2918fdc76bd3eba14dc3244221
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2022-12-30 10:50:38 -0500

    code: updated batch.R to run labs for env case

--- scripts/batch.R
+++ scripts/batch.R
@@ -8,18 +8,19 @@ library(ochoalabtools)
 #partition <- 'ochoalab'
 # draw partitions probabilistically!
 partitions <- c( 'ochoalab', 'biostat' )
-partitions_probs <- c(0.4, 0.6)
+# note this works without normalizing!
+partitions_probs <- c(2, 5)
 
 # shared items for all runs
 herit_low <- FALSE
-env <- FALSE
+env <- TRUE
 king_cutoff <- TRUE
 #bfile <- 'sim-n100-k10-f0.1-s0.5-g1'; short_orig <- 's'
 #bfile <- 'sim-n1000-k10-f0.1-s0.5-g1'; short_orig <- 'l'
 #bfile <- 'sim-n1000-k10-f0.1-s0.5-g20'; short_orig <- 'f'
-bfile <- 'HoPacAll_ld_prune_1000kb_0.3_maf-0.01'; short_orig <- 'h'
+#bfile <- 'HoPacAll_ld_prune_1000kb_0.3_maf-0.01'; short_orig <- 'h'
 #bfile <- 'hgdp_wgs_autosomes_ld_prune_1000kb_0.3_maf-0.01_geno-0.1'; short_orig <- 'd'
-#bfile <- 'tgp-nygc-autosomes_ld_prune_1000kb_0.3_maf-0.01'; short_orig <- 'k'
+bfile <- 'tgp-nygc-autosomes_ld_prune_1000kb_0.3_maf-0.01'; short_orig <- 'k'
 #bfile <- 'HoPacAll_ld_prune_1000kb_0.3_maf-0.01_sim'; short_orig <- 'H'
 #bfile <- 'hgdp_wgs_autosomes_ld_prune_1000kb_0.3_maf-0.01_geno-0.1_sim'; short_orig <- 'D'
 #bfile <- 'tgp-nygc-autosomes_ld_prune_1000kb_0.3_maf-0.01_sim'; short_orig <- 'K'
@@ -44,7 +45,7 @@ if ( king_cutoff ) {
 
 # main submission steps
 # uses array jobs, arraying over PCs
-submit_rep_pcs <- function( rep, bfile, threads, fes, herit_low, env, plink, partition ) {
+submit_rep_pcs <- function( rep, bfile, threads, fes, herit_low, env, plink, partition, labs = FALSE ) {
     # GCTA uses more memory, this works for the largest cases
     mem <- if ( plink ) '4G' else '16G'
 
@@ -81,8 +82,12 @@ submit_rep_pcs <- function( rep, bfile, threads, fes, herit_low, env, plink, par
         commands <- paste0( commands, ' --herit 0.3 --m_causal_fac 27' )
     
     # a pair of parameters for env simulations
-    if ( env )
+    if ( env ) {
         commands <- paste0( commands, ' --env1 0.3 --env2 0.2' )
+        # labs only makes sens for env and gcta
+        if ( labs && !plink )
+            commands <- paste0( commands, ' -l' )
+    }
     
     # load plink module if needed
     if ( plink ) {
@@ -117,56 +122,66 @@ submit_rep_pcs <- function( rep, bfile, threads, fes, herit_low, env, plink, par
 
 # automatically loop through main sims
 for ( plink in plink_cases ) {
-    for ( fes in fes_cases ) {
-        # set some settings/etc automatically fron here down
+    # decide labs situation
+    # most of the time there's no labs option
+    labs_cases <- FALSE
+    # labs needs GCTA and env (global)
+    if ( !plink && env )
+        labs_cases <- c(FALSE, TRUE)
 
-        ############
-        ### NAME ###
-        ############
-
-        # reset `short` name after every loop, otherwise it gets messed up!
-        short <- short_orig
+    for ( labs in labs_cases ) {
         
-        # set one of the levels of type of simulation in output name, so they don't overlap
-        if ( env ) {
-            # env only makes sense in combination with low heritability
-            herit_low <- TRUE
-            short <- paste0( short, 'e' ) # env (+ low-herit implied)
-        } else if ( herit_low ) {
-            short <- paste0( short, 'l' ) # low herit
-        } else {
-            short <- paste0( short, 'h' ) # high herit
-        }
-        
-        # add trait type marker to output
-        short <- paste0( short, if ( fes ) 'f' else 'r' )
+        for ( fes in fes_cases ) {
+            # set some settings/etc automatically fron here down
 
-        # so names don't overlap between plink and gcta runs
-        short <- paste0( short, if ( plink ) 'p' else 'g' )
+            ############
+            ### NAME ###
+            ############
 
-        ###########
-        ### ETC ###
-        ###########
-        
-        ## # I
-        ## # test one rep
-        ## rep <- 1L
-	## partition <- sample( partitions, 1L, prob = partitions_probs )
-        ## submit_rep_pcs( rep, bfile, threads, fes, herit_low, env, plink, partition )
+            # reset `short` name after every loop, otherwise it gets messed up!
+            short <- short_orig
+            
+            # set one of the levels of type of simulation in output name, so they don't overlap
+            if ( env ) {
+                # env only makes sense in combination with low heritability
+                herit_low <- TRUE
+                short <- paste0( short, 'e' ) # env (+ low-herit implied)
+            } else if ( herit_low ) {
+                short <- paste0( short, 'l' ) # low herit
+            } else {
+                short <- paste0( short, 'h' ) # high herit
+            }
+            
+            # add trait type marker to output
+            short <- paste0( short, if ( fes ) 'f' else 'r' )
 
-        ## # II
-        ## # finish rest of reps
-        ## for ( rep in 2L : reps_max ) {
-	##     partition <- sample( partitions, 1L, prob = partitions_probs )
-        ##     submit_rep_pcs( rep, bfile, threads, fes, herit_low, env, plink, partition )
-        ## }
+            # so names don't overlap between plink and gcta runs
+            short <- paste0( short, if ( plink ) 'p' else if ( labs ) 'l' else 'g' )
 
-        ########################
+            ###########
+            ### ETC ###
+            ###########
+            
+            ## # I
+            ## # test one rep
+            ## rep <- 1L
+            ## partition <- sample( partitions, 1L, prob = partitions_probs )
+            ## submit_rep_pcs( rep, bfile, threads, fes, herit_low, env, plink, partition, labs )
 
-        # start loop
-        for ( rep in 1L : reps_max ) {
-            partition <- sample( partitions, 1L, prob = partitions_probs )
-            submit_rep_pcs( rep, bfile, threads, fes, herit_low, env, plink, partition )
+            ## # II
+            ## # finish rest of reps
+            ## for ( rep in 2L : reps_max ) {
+            ##     partition <- sample( partitions, 1L, prob = partitions_probs )
+            ##     submit_rep_pcs( rep, bfile, threads, fes, herit_low, env, plink, partition, labs )
+            ## }
+
+            ########################
+
+            # start loop
+            for ( rep in 1L : reps_max ) {
+                partition <- sample( partitions, 1L, prob = partitions_probs )
+                submit_rep_pcs( rep, bfile, threads, fes, herit_low, env, plink, partition, labs )
+            }
         }
     }
 }
